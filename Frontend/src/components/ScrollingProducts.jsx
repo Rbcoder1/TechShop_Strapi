0f9935ebe7142c66bdedcaa696521774ac133a27
Rohit Borse <codingwithrb@gmail.com>
2023-07-15 16:45:17 +0530

    best seller slider added

--- Frontend/src/components/ScrollingProducts.jsx
+++ Frontend/src/components/ScrollingProducts.jsx
@@ -3,13 +3,12 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import '../Style/ScrollingProduct.css'
-import TempProduct from './TempProduct'
+import SingleProduct from './SingleProduct';
 
 const NextButton = (props) => {
   const { className, onClick } = props
   return (
     <div className={className} onClick={onClick} >
-      {/* <ArrowForwardIosIcon  style={{color : "white", fontSize : "30px"}} /> */}
       <i className="fa fa-angle-right" style={{ color: "blue", fontSize: "30px" }}></i>
 
     </div>
@@ -19,34 +18,44 @@ const PrevButton = (props) => {
   const { className, onClick } = props
   return (
     <div className={className} onClick={onClick}>
-      {/* <ArrowBackIosIcon style={{ color: "blue", fontSize: "30px" }} /> */}
       <i className="fa fa-angle-left" style={{ color: "blue", fontSize: "30px" }}></i>
     </div>
   )
 }
 
+const sliderProperties = {
+  prevArrow: <PrevButton />,
+  nextArrow: <NextButton />,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  infinite: true,
+  responsive:
+    [{
+      breakpoint: 426,
+      settings: {
+        slidesToShow: 1,
+        contentMode: false
+      }
+    },
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 3,
+        contentMode: false
+      }
+    }]
+}
 
 const ScrollingProducts = (props) => {
-  const [Product, setProduct] = useState({ product: [] })
-  const url = `http://localhost:5000/product/query-fetch?label=${props.category}`
-  useEffect(() => {
-    fetch(url)
-      .then(res => res.json())
-      .then((data) => {
-        setProduct({ product: data })
-      })
-  },[])
-  // console.log(Product)
   return (
-    <div className='container p-0'>
-      <Slider
-        slidesToShow={5}
-        nextArrow={<NextButton />}
-        prevArrow={<PrevButton />}
-      >
-        {Product.product.map((e) => {
-          return <TempProduct key={e._id} label={e.label} title={e.title} desc={e.description} price={e.price} />
-        })}
+    <div className='p-0'>
+      <Slider {...sliderProperties}>
+        <SingleProduct />
+        <SingleProduct />
+        <SingleProduct />
+        <SingleProduct />
+        <SingleProduct />
+        <SingleProduct />
       </Slider>
     </div>
   )
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import ScrollingProducts from '../components/ScrollingProducts'
+import '../Style/Home.css'
 
 const Home = () => {
   return (
-    <></>
+    <div style={{ 'marginTop': '70px', 'padding': '10px 20px' }} >
+      <div className="best-selling-conn">
+        <h3 className='heading-best-selling'>Best Selling</h3>
+        <span className='sub-heading-selling'>Everything That Trends Listed Here</span>
+        <ScrollingProducts />
+      </div>
+    </div>
   )
 }
 
